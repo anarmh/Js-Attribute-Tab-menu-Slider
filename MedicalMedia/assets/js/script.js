966c1e6ecf4cf068387cf5fabd07998de7df9d31
anarmh <anarmh@code.edu.az>
2023-02-28 22:59:02 +0400

    add practice code

--- MedicalMedia/assets/js/script.js
+++ MedicalMedia/assets/js/script.js
@@ -2,21 +2,22 @@
 
 let leftIcon=document.querySelector("#news-card .title .left");
 let rightIcon=document.querySelector("#news-card .title .right");
-
+let carouselItem
 function rightCarousel(){
     let activeCard=document.querySelector(".active")
-    activeCard.classList.remove("active")
+    // activeCard.classList.remove("active")
     if(activeCard.nextElementSibling!=null){
-        activeCard.nextElementSibling.classList.add("active")
+        activeCard.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.classList.add("active")
+        activeCard.parentNode.firstElementChild.classList.remove("active")
     }
     else{
-        activeCard.parentNode.firstElementChild.classList.add("active")
+        
     }
 
 }
 function leftCarousel(){
     let activeCard=document.querySelector(".active")
-    activeCard.classList.remove("active")
+    // activeCard.classList.remove("active")
     if(activeCard.previousElementSibling!=null){
         activeCard.previousElementSibling.classList.add("active")
     }
---
+++ Practice/assets/js/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// let  link=document.querySelector("a");
+
+// let linkedinBtn=document.querySelector(".linkedin");
+// let googleBtn=document.querySelector(".google");
+
+
+// googleBtn.addEventListener("click",function(){
+//     link.setAttribute("href","https://www.google.com/")
+//     link.setAttribute("target","_blank")
+//     link.click()
+// })
+// linkedinBtn.addEventListener("click",function(){
+//     link.setAttribute("href","https://www.linkedin.com/")
+//     link.click()
+// })
+
+// tab-menu
+// let menues = document.querySelectorAll(".headers .item");
+// let contents = document.querySelectorAll(".contents .item-text")
+// menues.forEach(menu => {
+//     menu.addEventListener("click", function (e) {
+//         document.querySelector(".active").classList.remove("active")
+//         e.target.classList.add("active")
+
+//         contents.forEach(content => {
+//             if (this.getAttribute("data-id") == content.getAttribute("data-id")) {
+//                 content.classList.remove("d-none");
+//             }
+//             else {
+//                 content.classList.add("d-none");
+//             }
+
+//         });
+//     })
+// });
+
+// slider
+
+
